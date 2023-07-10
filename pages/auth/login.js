4425c8e7db7a74ab9afee1a01744263207d001c7
Jon Sundin <jsundin91@gmail.com>
2023-07-10 13:22:15 -0700

    added login page

---
+++ pages/auth/login.js
@@ -0,0 +1,154 @@
+import Link from "next/link";
+
+function Login() {
+  return (
+    <section className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <div className="hidden lg:relative lg:block lg:p-12">
+            <Link className="block text-white" href="/">
+              <span className="sr-only">Home</span>
+              <img
+                className="m-2 col-span-4"
+                width="80"
+                height="80"
+                src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Quote_Mining_Fallacy_Icon.png"
+                alt="Quoted"
+              />
+              <h1 className="col-span-4 mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                Quoted!
+              </h1>
+            </Link>
+
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Quoted!
+            </h2>
+
+            <p className="mt-4 leading-relaxed text-white/90">
+              A simple app to share quotes with your friends.
+            </p>
+          </div>
+        </section>
+
+        <main className="flex items-center justify-center w-full px-2 py-2 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+          <div className="max-w-xl lg:max-w-3xl">
+            <div className="relative block lg:hidden">
+              <Link
+                className="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
+                href="/"
+              >
+                <span className="sr-only">Home</span>
+                <img
+                  width="80"
+                  height="80"
+                  src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Quote_Mining_Fallacy_Icon.png"
+                  alt="Quoted"
+                />
+              </Link>
+
+              <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+                Quoted!
+              </h1>
+
+              <p className="mt-4 leading-relaxed text-gray-500">
+                A social network for sharing quotes!
+              </p>
+            </div>
+
+            <form action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+              <div>
+                <label
+                  for="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+
+                <div className="relative">
+                  <input
+                    type="email"
+                    className="w-full rounded-lg border-solid border-2 border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                  />
+
+                  <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4 text-gray-400"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <label
+                  for="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+
+                <div className="relative">
+                  <input
+                    type="password"
+                    className="w-full rounded-lg border-solid border-2 border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                  />
+
+                  <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4 text-gray-400"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <p className="mt-4 text-base text-gray-500 sm:mt-0">
+                  No account?
+                  <a className="text-gray-700 underline m-2" href="/auth/signup">
+                    Sign up
+                  </a>
+                </p>
+
+                <button
+                  type="submit"
+                  className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-base font-medium text-white"
+                >
+                  Log in
+                </button>
+              </div>
+            </form>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
