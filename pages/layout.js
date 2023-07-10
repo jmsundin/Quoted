102c014d9670fb0b228d9c462c0ce7c4d1fda9ff
Jon Sundin <jsundin91@gmail.com>
2023-07-10 14:55:46 -0700

    removed metadata

--- pages/layout.js
+++ pages/layout.js
@@ -1,11 +1,6 @@
 // These styles apply to every route in the application
 import "./globals.css";
 
-export const metadata = {
-  title: "Quoted",
-  description: "A simple app to share quotes with your friends.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
