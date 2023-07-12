056e397dc3e958449c2fd854df01527df1d62152
Jon Sundin <jsundin91@gmail.com>
2023-07-11 17:51:35 -0700

    made pathname general rather than specific

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'upload.wikimedia.org',
-            port: '',
-            pathname: '/wikipedia/commons/**/*',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+        pathname: "/**/*",
       },
-}
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
