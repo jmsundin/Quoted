3950435553c47a6f41de8ef5528e79f590d9224e
Jon Sundin <jsundin91@gmail.com>
2023-07-11 17:37:53 -0700

    added _app.js to override index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import "./globals.css";
+import { AuthContextProvider } from "@/context/AuthContext";
+import { usePathname } from "next/navigation";
+import ProtectRoute from "@/components/ProtectedRoute";
+
+const noAuthRoutes = ["/", "/auth/login", "/auth/signup"];
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const pathname = usePathname();
+
+  return (
+    <AuthContextProvider>
+      {noAuthRoutes.includes(pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <ProtectRoute>
+          <Component {...pageProps} />
+        </ProtectRoute>
+      )}
+    </AuthContextProvider>
+  );
 }
 
 export default MyApp;
