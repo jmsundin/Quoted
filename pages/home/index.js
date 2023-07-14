93c5b67592b4ba383f2f469686acdbd7915c3ca1
Jon Sundin <jsundin91@gmail.com>
2023-07-14 15:47:35 -0700

    Head and MainNav into HomePage

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,14 +1,32 @@
-import React from "react";
-import Header from "@/components/Header";
-import QuoteCard from "@/components/QuoteCard";
+import { Fragment } from "react";
+import Head from "next/head";
+
+// import UserPosts from "@/components/posts/user-posts";
+// import { getUserPosts } from "@/lib/db";
+
+import MainNavigation from "@/components/layout/main-navigation";
 
 function HomePage() {
   return (
-    <React.Fragment>
-      <Header />
-      <QuoteCard />
-    </React.Fragment>
+    <Fragment>
+      <Head>
+        <title>Quoted | Home</title>
+        <meta name="description" content="Quoted Home Page" />
+      </Head>
+      <MainNavigation />
+    </Fragment>
   );
 }
+
+// export function getStaticProps() {
+//   // const posts = await getUserPosts();
+//   // console.log(posts);
+//   return {
+//     props: {
+//       // posts: posts,
+//     },
+//     revalidate: 60,
+//   };
+// }
 
 export default HomePage;
