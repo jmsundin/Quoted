65d3a3b27014623928688c09094709cbc25adc6d
Jon Sundin <jsundin91@gmail.com>
2023-07-24 14:56:12 -0700

    add Head component to signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,19 @@
 "use client";
 
+import { Fragment } from "react";
+import Head from "next/head";
+
 import SignupForm from "@/components/auth/SignupForm";
 
 function SignupPage() {
-  return <SignupForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Quoted | Signup</title>
+        <meta name="description" content="Quoted Signup Page" />
+      </Head>
+      <SignupForm />;
+    </Fragment>
+  );
 }
 export default SignupPage;
