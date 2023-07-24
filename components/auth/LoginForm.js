70971b2829c1ee0b8fd6d68976e9ce57e851f44b
Jon Sundin <jsundin91@gmail.com>
2023-07-24 14:57:35 -0700

    organizing lib - changed import path

--- components/auth/LoginForm.js
+++ components/auth/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import { useRouter, usePathname } from "next/navigation";
-import { useAuthContext } from "@/context/AuthContext";
+import { useAuthContext } from "@/lib/context/AuthContext";
 
 import Link from "next/link";
 import Image from "next/image";
