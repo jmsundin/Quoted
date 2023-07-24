9622f727354e5c70a13fa8e4296b4ee5c75f14ca
Jon Sundin <jsundin91@gmail.com>
2023-07-24 15:01:57 -0700

    implemented main nav

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,89 +1,89 @@
 "use client";
 
-import React from "react";
+import { useState, useEffect } from "react";
 
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
 import Image from "next/image";
-
+import Link from "next/link";
 import Dropdown from "../Dropdown";
 
+import { useAuthContext } from "@/lib/context/AuthContext";
+import MyFirebase from "@/lib/firebase/MyFirebase";
+import { ref, getDownloadURL } from "firebase/storage";
+import { collection, doc, getDoc } from "firebase/firestore";
+
 import Logo from "./Logo";
+import { MdAccountCircle } from "react-icons/md";
 
 function MainNavigation() {
+  const { user } = useAuthContext();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [userData, setUserData] = useState({});
+  const [profilePhotoUrl, setProfilePhotoUrl] = useState("");
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const usersCollectionRef = collection(MyFirebase.db, "users");
+      const userRef = doc(usersCollectionRef, user.uid);
+      let userDoc = null;
+      try {
+        userDoc = await getDoc(userRef);
+      } catch (e) {
+        console.error(e);
+      }
+      try {
+        setUserData(userDoc.data());
+      } catch (e) {
+        console.error(e);
+      }
+      try {
+        setProfilePhotoUrl(userDoc.data().profilePhotoUrl);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getUserData();
+  }, [user.uid, profilePhotoUrl]);
+
   return (
-    <header className="bg-gray-50">
-      <div className="flex-no-wrap relative flex w-full items-center justify-between gap-4 mx-auto max-w-screen-xl px-4 py-4 sm:px-6 lg:px-8">
-        <div className="flex-1 place-content-start gap-4">
+    <header className="bg-gray-100">
+      <div className="flex flex-row relative w-full items-center justify-between gap-4 mx-auto max-w-screen-xl px-4 py-4 sm:px-6 lg:px-8">
+        <div className="flex-initial place-content-start gap-4">
           <Logo />
         </div>
-        <div className="flex place-content-end align-middle gap-4">
-          {/* <div className="relative">
-              <label className="sr-only" htmlFor="search">
-                {" "}
-                Search{" "}
-              </label>
-
-             <input
-                className="h-10 w-full rounded-full border-none bg-white pe-10 ps-4 text-sm shadow-sm sm:w-56"
-                id="search"
-                name="search"
-                type="search"
-                placeholder="Search website..."
-              /> 
-
-              <button
-                type="button"
-                className="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
-              >
-                <span className="sr-only">Search</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
-            </div> */}
-
-          <Link href="/profile" className="shrink-0 flex align-middle">
-            <span className="sr-only">Profile picture</span>
-            <Image
-              alt="Man"
-              width={40}
-              height={40}
-              src="/default-profile-icon.png"
-              className="h-10 w-10 rounded-full object-cover"
-            />
-          </Link>
-
+        {pathname === "/home" ? (
           <Link
-            href="/notifications"
-            className="shrink-0 rounded-full bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
+            href="/create-post"
+            className="flex-auto grid justify-items-end invisible sm:visible"
           >
-            <span className="sr-only">Notifications</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
+            <button
+              type="button"
+              className="inline-block rounded-full bg-green-300 px-5 py-3 
+      shadow-[0_4px_9px_-4px_#3b71ca] 
+      transition duration-150 ease-in-out hover:bg-green-400 
+      hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] "
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              Create Post
+            </button>
+          </Link>
+        ) : null}
+
+        <div className="flex place-content-end items-center gap-4">
+          <Link href="/profile" className="shrink-0 flex">
+            <span className="sr-only">Profile picture</span>
+            {!profilePhotoUrl ? (
+              <MdAccountCircle className="h-12 w-12 rounded-full object-cover text-blue-500" />
+            ) : (
+              <Image
+                src={profilePhotoUrl}
+                width={40}
+                height={40}
+                className="flex w-12 h-12 rounded-full object-cover text-blue-500"
+                alt="Profile Photo"
               />
-            </svg>
+            )}
           </Link>
           <Dropdown />
         </div>
