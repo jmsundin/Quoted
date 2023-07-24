d7cf8d35382394afc722505ab9af020361be4d88
Jon Sundin <jsundin91@gmail.com>
2023-07-24 15:04:46 -0700

    changed sizing of logo and made flex-none

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -3,18 +3,24 @@ import Image from "next/image";
 
 function Logo() {
   return (
-    <Link
-      className="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
-      href="/"
-    >
-      <span className="sr-only">Home</span>
-      <Image
-        width="80"
-        height="80"
-        src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Quote_Mining_Fallacy_Icon.png"
-        alt="Quoted"
-      />
-    </Link>
+    <div className="flex flex-row items-center justify-start w-50 h-40">
+      <Link
+        className="flex-none bg-white sm:h-20 sm:w-20"
+        href="/"
+      >
+        <span className="sr-only">Home</span>
+        <Image
+          width={60}
+          height={60}
+          src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Quote_Mining_Fallacy_Icon.png"
+          alt="Quoted"
+          className="rounded-full"
+        />
+        </Link>
+        <Link href="/" className="flex-auto justify-start ml-2 text-2xl font-bold text-gray-700">
+        <span >Quoted!</span>
+      </Link>
+    </div>
   );
 }
 
