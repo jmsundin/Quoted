93dcdf753eff00dd22263228816929be11a214f3
Jon Sundin <jsundin91@gmail.com>
2023-07-24 19:18:17 -0700

    fixed logo responsiveness

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -3,23 +3,25 @@ import Image from "next/image";
 
 function Logo() {
   return (
-    <div className="flex flex-row items-center justify-start w-50 h-40">
+    <div className="flex flex-auto flex-row flex-wrap items-center justify-start gap-2">
+      <div className="flex flex-auto flex-row flex-wrap items-center justify-center gap-2">
       <Link
-        className="flex-none bg-white sm:h-20 sm:w-20"
-        href="/"
+        className="flex-initial"
+        href="/home"
       >
         <span className="sr-only">Home</span>
         <Image
-          width={60}
-          height={60}
+          width={64}
+          height={64}
           src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Quote_Mining_Fallacy_Icon.png"
           alt="Quoted"
-          className="rounded-full"
+          className="shrink-0 h-16 w-16 flex-none justify-center items-center rounded-full object-cover"
         />
         </Link>
-        <Link href="/" className="flex-auto justify-start ml-2 text-2xl font-bold text-gray-700">
+        <Link href="/home" className="flex flex-auto justify-center text-2xl font-bold text-gray-700">
         <span >Quoted!</span>
       </Link>
+      </div>
     </div>
   );
 }
