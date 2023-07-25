3d8ed2a7def49ec8dae40a2b50049bc38f1e5dd8
Jon Sundin <jsundin91@gmail.com>
2023-07-24 19:16:51 -0700

    removed useEffect from Home page

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
-
-import { useAuthContext } from "@/lib/context/AuthContext";
-import MyFirebase from "@/lib/firebase/MyFirebase";
-import { collection, getDocs } from "firebase/firestore";
+import { Fragment } from "react";
 
 import Head from "next/head";
 import Link from "next/link";
@@ -11,22 +7,6 @@ import CreatePostButton from "@/components/ui/CreatePostButton";
 import Posts from "@/components/Posts";
 
 function HomePage() {
-  const { user } = useAuthContext();
-  const [isLoading, setIsLoading] = useState(true);
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    async function getPosts() {
-      const postsCol = collection(MyFirebase.db, "posts");
-      const postsSnapshot = await getDocs(postsCol);
-      const postsList = postsSnapshot.docs.map((doc) => doc.data());
-      setPosts(postsList);
-    }
-    getPosts();
-    setIsLoading(false);
-  }, []);
-
   return (
     <Fragment>
       <Head>
@@ -34,9 +14,12 @@ function HomePage() {
         <meta name="description" content="Quoted Home Page" />
       </Head>
       <MainNavigation />
-      {isLoading && <p>Loading...</p>}
+
       <Posts />
-      <Link href="/create-post" className="fixed bottom-0 right-0 m-2 visible sm:invisible">
+      <Link
+        href="/create-post"
+        className="fixed bottom-0 right-0 m-2 visible sm:invisible"
+      >
         <CreatePostButton />
       </Link>
     </Fragment>
